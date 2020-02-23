235237e0c31386763a71f23e40139c237f73fc68
Simon Eddy <simon@simoneddy.com.au>
2020-02-23 19:50:31 +1100

    more tests

--- test/ScaleCalculator.test.js
+++ test/ScaleCalculator.test.js
@@ -1,12 +1,24 @@
 const { assert } = require('chai');
 const ScaleCalculator = require('../ScaleCalculator');
 
+const calc = new ScaleCalculator();
+
 describe('ScaleCalculator', () => {
   describe('#calculateOctaveRange()', () => {
     it('should calculate the frequencies of each note for the given octave range', () => {
-      assert.equal(new ScaleCalculator().calculateOctaveRange(4, 5).length, 24);
-      assert.equal(new ScaleCalculator().calculateOctaveRange(0, 8).length, 108);
+      assert.equal(calc.calculateOctaveRange(4, 5).length, 24);
+      assert.equal(calc.calculateOctaveRange(0, 8).length, 108);
+      assert.equal(calc.calculateOctaveRange(2, 5)[0].note, 'C2');
     });
+  })
+  describe('#calculatePiano()', () => {
+    it('should return octaves between 0 and 8', () => {
+      const piano = calc.calculatePiano(); 
+      assert.equal(piano.length, 108);
+      assert.equal(piano[0].note, 'C0');
+      assert.equal(piano[107].note, 'B8');
+    });
+    
   })
   
 })
